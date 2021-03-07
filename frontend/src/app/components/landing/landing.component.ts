34a11cf938779e34a9cc145fe4cdbcbc67b96bf0
Pramesh Bajracharya <pe.messh@gmail.com>
2021-03-07 16:14:01 +0545

    Starting frontend.

--- frontend/src/app/components/landing/landing.component.ts
+++ frontend/src/app/components/landing/landing.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
+import { CognitoUserInterface } from '@aws-amplify/ui-components';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
+
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+    currentUser: CognitoUserInterface | null;
+    email: String;
 
-  ngOnInit(): void {
-  }
+    constructor(private authService: AuthService) { }
+
+    ngOnInit(): void {
+        this.authService.getCurrentUser().subscribe((user: CognitoUserInterface) => {
+            console.log(user);
+            this.currentUser = user;
+            this.email = this.currentUser.attributes.email;
+        }, (error) => {
+            console.error(error);
+        });
+    }
 
 }
--- frontend/src/app/components/resisterlogin/resisterlogin.component.ts
+++ frontend/src/app/components/resisterlogin/resisterlogin.component.ts
@@ -2,26 +2,29 @@ import { ChangeDetectorRef } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { onAuthUIStateChange, CognitoUserInterface, AuthState } from '@aws-amplify/ui-components';
 
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-resisterlogin',
   templateUrl: './resisterlogin.component.html',
   styleUrls: ['./resisterlogin.component.scss']
 })
+
 export class ResisterloginComponent implements OnInit {
 
     public authState: AuthState;
     public user: CognitoUserInterface | undefined;
 
-    constructor(private ref: ChangeDetectorRef) { }
+    constructor(private ref: ChangeDetectorRef, private authService: AuthService) { }
 
     ngOnInit(): void {
         onAuthUIStateChange((authState, authData) => {
             this.authState = authState;
             this.user = authData as CognitoUserInterface;
+            this.authService.setCurrentUser(this.user);
             this.ref.detectChanges();
-            console.log({ authState }, { authData });
-        })
+        });
     }
 
 }
