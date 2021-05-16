588bf2351c6129b07f1af4453522323ca6587f99
Pramesh Bajracharya <pe.messh@gmail.com>
2021-05-16 22:25:44 +0545

    Register Users done.

--- backend/src/lambdas/http/registerUser.js
+++ backend/src/lambdas/http/registerUser.js
@@ -1,15 +1,33 @@
 'use strict';
 
+const AWS = require("aws-sdk");
+const COGNITO = new AWS.CognitoIdentityServiceProvider();
+const USER_POOL_ID = process.env.COGNITO_USER_POOL_ID
+
 module.exports.handler = async (event) => {
+    try {
+        const newUserCreate = await COGNITO.adminCreateUser({
+            Username: "pe.messh@gmail.com",
+            UserPoolId: USER_POOL_ID,
+            UserAttributes: [
+                {
+                    Name: "email",
+                    Value: "pe.messh@gmail.com"
+                }
+            ]
+        }).promise().then(async data => {
+            console.log("************* Data from promise resolve: ", data);
+        }).catch(err => {
+            console.log("************ Error from promise resolve: ", err);
+        });
+        console.log("------------------------- SUCCESS ----------------------------");
+        console.log(newUserCreate);
+    } catch (error) {
+        console.log("------------------------- ERROR ------------------------------");
+        console.log(error)
+    }
     return {
         statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: 'Go Serverless v1.0! Your function executed successfully!',
-                input: event,
-            },
-            null,
-            2
-        ),
+        body: JSON.stringify({ "Message": "DONE" }),
     };
 };
